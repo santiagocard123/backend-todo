c6d0ae4e8fd8b7901e6c49688e854d8dbe7b19fc
Santiago Cardenas <Santiago.Cardenas@jala.university>
2025-04-15 12:20:59 -0500

    second commit

--- src/main/java/jala/university/todo/controller/UserController.java
+++ src/main/java/jala/university/todo/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "https://.vercel.app")
+@CrossOrigin(origins = "https://frontend-todo-gilt.vercel.app/")
 public class UserController {
 
     private final UserService userService;
